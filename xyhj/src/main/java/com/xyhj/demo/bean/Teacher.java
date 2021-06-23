b97242d719cb08afe2c581cf1678b496a0cb5e3f
peng-q-h <pengquanhong@163.com>
2021-06-23 14:02:08 +0800

    添加实体类teacher

---
+++ xyhj/src/main/java/com/xyhj/demo/bean/Teacher.java
@@ -0,0 +1,14 @@
+package com.xyhj.demo.bean;
+
+import lombok.Data;
+
+/**
+ * @Author: pengquanhong
+ * @Date: 2021/6/23 14:01
+ * @Description:
+ */
+@Data
+public class Teacher {
+	private String code;
+	private String name;
+}
