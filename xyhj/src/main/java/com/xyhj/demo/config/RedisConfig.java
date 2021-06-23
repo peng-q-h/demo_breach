45a648d8a03a78d411cc23bcd1eb55c2400dd1eb
peng-q-h <pengquanhong@163.com>
2021-06-23 10:43:39 +0800

    hehe

---
+++ xyhj/src/main/java/com/xyhj/demo/config/RedisConfig.java
@@ -0,0 +1,12 @@
+package com.xyhj.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author: pengquanhong
+ * @Date: 2021/6/23 10:42
+ * @Description:
+ */
+@Configuration
+public class RedisConfig {
+}
