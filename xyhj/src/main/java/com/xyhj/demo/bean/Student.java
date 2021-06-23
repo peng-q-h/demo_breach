985f0d2d1d52af00b16931787f417fe3243c20dd
peng-q-h <pengquanhong@163.com>
2021-06-23 10:48:27 +0800

    添加实体类student

---
+++ xyhj/src/main/java/com/xyhj/demo/bean/Student.java
@@ -0,0 +1,14 @@
+package com.xyhj.demo.bean;
+
+import lombok.Data;
+
+/**
+ * @Author: pengquanhong
+ * @Date: 2021/6/23 10:45
+ * @Description:
+ */
+@Data
+public class Student {
+	private String id;
+	private String name;
+}
